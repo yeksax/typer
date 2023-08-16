a41162eaa4585adaa38981164b58410d38f5d683
yeskax <hugoalmeida2412@gmail.com>
2023-08-15 23:32:58 -0300

    feat: added redirect to /typer post signin

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,7 +1,19 @@
-<script>
+<script lang="ts">
   import { signIn } from "@auth/sveltekit/client";
   import { Icon } from "svelte-awesome";
   import { github, google } from "svelte-awesome/icons";
+
+  function signWithGoogle() {
+    signIn("google", {
+      callbackUrl: "/typer",
+    });
+  }
+
+  function signWithGithub() {
+    signIn("github", {
+      callbackUrl: "/typer",
+    });
+  }
 </script>
 
 <div class="grid max-md:place-items-center h-full">
@@ -14,13 +26,13 @@
 
     <div class="flex flex-col gap-3">
       <button
-        on:click={() => signIn("google")}
+        on:click={signWithGoogle}
         class="flex px-6 dark:bg-zinc-750 dark:hover:bg-zinc-900 transition-all py-2 gap-4 items-center rounded-md border-2 border-b-4 border-black dark:border-zinc-950">
         <Icon data={google} scale={1.5} />
         Continuar com Google
       </button>
       <button
-        on:click={() => signIn("github")}
+        on:click={signWithGithub}
         class="flex px-6 dark:bg-zinc-750 py-2 gap-4 dark:hover:bg-zinc-900 transition-all items-center rounded-md border-2 border-b-4 border-black dark:border-zinc-950">
         <Icon data={github} scale={1.5} />
         Continuar com Github
