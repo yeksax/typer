88408eb59f5b8ff500f644b8433ed837674c0916
yeskax <hugoalmeida2412@gmail.com>
2023-08-18 21:09:20 -0300

    feat: added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,6 +12,11 @@
   import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query";
   import { onMount } from "svelte";
 
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
+  inject({ mode: dev ? "development" : "production" });
+
   $theme = $page.data.theme ?? "SYSTEM_DEFAULT";
 
   $: {
